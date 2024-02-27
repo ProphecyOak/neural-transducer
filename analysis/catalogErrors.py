5f37e4771c5874cdde06aa80bdd9ff84f18b8806
ProphecyOak <drito610@gmail.com>
2024-02-27 13:57:11 -0500

    now with more functionality :)

--- analysis/catalogErrors.py
+++ analysis/catalogErrors.py
@@ -1,18 +1,33 @@
 import sys
 import re
 
-with open("../checkpoints/sig23/tagtransformer/fra.decode.dev.tsv") as f:
-    contents = f.read()
+def sortOutErrors(fileToCheck="test", sortFunction=lambda x: x, language="fra"):
+    with open(f"../checkpoints/sig23/tagtransformer/{language}.decode.{fileToCheck}.tsv") as f:
+        contents = f.read()
 
-lines = contents.strip().split("\n")
-columns = []
-for line in lines[1:]:
-    columns.append(line.replace(" ","").split("\t"))
+    lines = contents.strip().split("\n")
+    columns = []
+    for line in lines[1:]:
+        columns.append(line.replace(" ","").split("\t"))
 
-errorColumns = list(filter(lambda x: x[3] != '0', columns))
-errorColumns.sort(key=lambda x: x[3],reverse=True)
+    errorColumns = list(filter(lambda x: x[3] != '0', columns))
+    if fileToCheck == "test": fileToCheck = "tst"  #Because for whatever reason, neural_transducer doesnt use same extension formatting :/
+    with open(f"../../2023InflectionST/part1/data/{language}.{fileToCheck}") as dat, open(f"errors.{language}.{fileToCheck}.tsv","w") as e, open(f"unexpectedForms.{language}.{fileToCheck}.tsv","w") as u:
+        lemmaDict = {}
+        e.write("lemma\t"+lines[0]+"\n")
+        u.write(lines[0]+"\n")
+        for form in dat.read().strip().split("\n"):
+            splitForm = re.split(r"\s+",form)
+            lemmaDict[splitForm[2]] = splitForm[0]
+        for line in errorColumns:
+            try:
+                line.insert(0,lemmaDict[line[1]])
+            except:
+                u.write("\t".join(line)+"\n")
+                errorColumns.remove(line)
+        errorColumns.sort(key=sortFunction)
+        for line in errorColumns:
+            if len(line) == 5: e.write("\t".join(line)+"\n")
 
-with open("errors.tsv","w") as e:
-    e.write(lines[0]+"\n")
-    for row in errorColumns:
-        e.write("\t".join(row)+"\n")
+if __name__ == "__main__":
+    sortOutErrors("test")
